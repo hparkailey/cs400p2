f5cead5e08b2863b259b80a6391498387624a59f
gsetyawan <54418994+gabysetyawan@users.noreply.github.com>
2021-03-09 16:53:56 -0600

    No need to put checkout

--- ReservationDataInterface.java
+++ ReservationDataInterface.java
@@ -3,7 +3,7 @@ public interface ReservationDataInterface extends Comparable<ReservationDataInte
     public Integer getRoomNumber();
     public String getName();
     public String getCheckInDate();
-    public String getCheckOutDate();
+    public String getCheckOutDate(); // I don't think there should be a getCheckoutDate() because we are not removing anything
 
     //from comparable
     public int compareTo(ReservationDataInterface otherReservation);
